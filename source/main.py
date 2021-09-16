0bfe72e5f5ae2aaff43a67804d4b612b92f14340
gshzero <gshzero@163.com>
2021-09-16 10:40:08 +0800

    修改概念函数，新增统计股票上涨天数占比

--- source/main.py
+++ source/main.py
@@ -10,7 +10,6 @@ from source.concept_date import Concept
 import time
 
 
-
 class main:
     def getAtockCount(self, days):
         print('-------Get Atock Count start-------')
@@ -27,19 +26,21 @@ class main:
         """
         数据层更新数据库日交易数据
         """
+        stock_klines_tupple = []
         with UsingMysql(log_time=True) as um:
             sql = "select id  from stock_klines where stock_number = %s " % (dict['code'])
             id = um.get_count(sql, None, 'id')
             print("-- 当前id: %s" % id)
             if id == 0:
                 sql = "INSERT INTO stock_klines ( stock_number, name,klines ) VALUES ( %s, %s,%s);" % (
-                "\"" + dict['code'] + "\"", "\'" + dict['name'] + "\'", "\"" + str(dict['klines']) + "\"")
+                    "\"" + dict['code'] + "\"", "\'" + dict['name'] + "\'", "\"" + str(dict['klines']) + "\"")
                 um.install_one(sql, None)
                 print('install success')
             else:
-                sql = "UPDATE stock_klines SET klines=%s WHERE id=%d" % ("\"" + str(dict['klines']) + "\"", id)
-                um.update_by_pk(sql)
-                print('update success')
+                stock_klines_tupple.append((str(dict['klines']), str(dict['name']), str(id)))
+            sql = "UPDATE stock_klines SET klines=(%s),name=(%s) WHERE id=(%s)"
+            um.update_many(sql, stock_klines_tupple)
+            print('update success')
 
     def update_stock_kline(self):
         """
@@ -61,29 +62,26 @@ class main:
 
         print('all stock update success')
 
-    def updata_stock_message(self, cookies):
+    def updata_stock_message(self):
         """
         更新数据库中股票概念、行业、地区信息
         """
         print('-----update stock message start-----')
         with UsingMysql(log_time=True) as um:
-            sql = "select id,stock_number from stock_list WHERE product = ',房地产,商品房,住宅地产,房地产业务,铁矿'"
-            id_list = um.fetch_all(sql, None,)
+            sql = "select id,stock_number from stock_list"
+            id_list = um.fetch_all(sql, None, )
             all_length = len(id_list)
             Serial_number = 0
             if all_length == 0:
                 print('not find id of atock' + sql)
                 sys.exit(1)
-            for stock_id in id_list:
-                # time.sleep(10)
-                stock_message = HtmlDataGet(stock_id["stock_number"], cookie=cookies)
-                stock_product = stock_message.get_product_list_element()
-                stock_concept = stock_message.get_concept_list_element()
-                stock_city = stock_message.get_city_element()
-                stock_ndustry = stock_message.get_ndustry_element()
-                sql = "update stock_list set product = '%s',concept = '%s',city = '%s',ndustry = '%s' where id= '%s';" % (stock_product, stock_concept, stock_city, stock_ndustry, stock_id["id"])
-                um.update_by_pk(sql, None)
-                serial_number = Serial_number + 1
+            else:
+                stock_concept_list = HtmlDataGet().get_stock_concept(id_list)
+                for concept in stock_concept_list:
+                    sql = "update stock_list set concept = '%s' where id= '%s';" % (concept['concept'], concept["id"])
+                    um.update_by_pk(sql, None)
+                    serial_number = Serial_number + 1
+                    print(serial_number / len(id_list) * 100)
             print('-----all stock updated-----')
 
     def get_atock_margin(self, days):
@@ -118,13 +116,14 @@ class main:
         plate_klines_list = http_connect.get_concept_klines(plate_list)
         if plate_klines_list is not None:
             with UsingMysql(log_time=True) as um:
+                um.delete('delete from concept_klines')
                 for i in plate_klines_list:
                     sql = "select id  from concept_klines where concept_number = '%s'" % (i['concept_number'])
                     id = um.get_count(sql, None, 'id')
                     print("-- 当前id: %s" % id)
                     if id == 0:
                         sql = "INSERT INTO concept_klines ( concept_number, concept_name,klines ) VALUES ( %s, %s,%s);" % (
-                        "\"" + i['concept_number'] + "\"", "\'" + i['概念名称'] + "\'", "\"" + i['klines'] + "\"")
+                            "\"" + i['concept_number'] + "\"", "\'" + i['概念名称'] + "\'", "\"" + i['klines'] + "\"")
                         um.install_one(sql, None)
                         print('install success')
                     else:
@@ -136,18 +135,56 @@ class main:
             print("concept_klines_list is null")
         print('all concept update success')
 
+    def get_concept_bottom(self, days):
+        print('-------get concept bottom start-------')
+        with UsingMysql(log_time=True) as um2:
+            sql = "select concept_number,concept_name,klines from concept_klines"
+            data = um2.fetch_all(sql, None)
+            concept_count = Concept()
+            concept_count_list = concept_count.get_concept_doji(data, days)
+            table = ExcelWrite('_concept_Doji', concept_count_list)
+            table.write_sheet()
+            table.save_book()
+
+    def get_rise_concept_list(self, days):
+        print('-------get concept bottom start-------')
+        with UsingMysql(log_time=True) as um2:
+            sql = "select concept_number,concept_name,klines from concept_klines"
+            data = um2.fetch_all(sql, None)
+            concept_count = Concept()
+            concept_count_list = concept_count.get_rise_concep(data, days)
+            table = ExcelWrite('_rise_concept_list', concept_count_list)
+            table.write_sheet()
+            table.save_book()
+
+    def get_atock_rise(self, days):
+        print('-------get atock rise list start-------')
+        with UsingMysql(log_time=True) as um3:
+            sql = "select a.stock_number,a.name,a.klines,b.concept from stock_klines a LEFT JOIN stock_list b ON a.stock_number = b.stock_number WHERE a.stock_number LIKE '000%' OR a.stock_number LIKE '600%' or a.stock_number LIKE '601%' or a.stock_number LIKE '603%' or a.stock_number LIKE '605%' or a.stock_number LIKE '688%' or a.stock_number LIKE '689%'"
+            data = um3.fetch_all(sql, None)
+            atock_count = atockTrendCount(data, days)
+            atock_count_list = atock_count.get_rise_stock()
+            table = ExcelWrite('_atock_rise', atock_count_list)
+            table.write_sheet()
+            table.save_book()
+
+
 if __name__ == '__main__':
     # 获取股票最近几天的支撑和案例对比数据
     # main().getAtockCount(5)
-    #更新数据库股票日交易数据
-    main().update_stock_kline()
-    #更新股票概念信息
+    # 更新数据库股票日交易数据
+    # main().update_stock_kline()
+    # 更新股票概念信息
     # Cookies = 'cid=9694472d4d82cd29fe1c071b36d4d3181627980027; ComputerID=9694472d4d82cd29fe1c071b36d4d3181627980027; WafStatus=0; other_uid=Ths_iwencai_Xuangu_bgee9foa6zwk1ksuqxbxbxdhwp5f26th; ta_random_userid=xgwoi1enwi; vvvv=1; PHPSESSID=9694472d4d82cd29fe1c071b36d4d318; v=A4cFB7L6qhelcC6bSVmvoZBJFjBSjFtutWDf4ll0o5Y9yKkmYVzrvsUwb3xq; '
-    # main().updata_stock_message(Cookies)
+    # main().updata_stock_message()
     # 获取上涨股票列表
     # main().get_atock_margin(4)
     # 获取十字星股票列表
-    # main().get_atock_bottom(4)
+    # main().get_atock_bottom(10)
     # 更新板块k线记录
     # main().update_concept_kline()
-
+    # 获取上涨趋势概念列表
+    #   main().get_concept_bottom(5)
+    # 统计最近几天上涨的概念
+    main().get_rise_concept_list(6)
+    # main().get_atock_rise(5)
---
+++ source/news_sina.py
@@ -0,0 +1,49 @@
+import requests
+import ast
+import json
+from source.tool import Tool
+
+from source.UsingMysql import UsingMysql
+from source.excle import ExcelWrite
+
+
+class news_finance(object):
+    def get_news(self, page):
+        """调用新浪财经新闻接口返回最新20条新闻
+        return：json格式最新20条新闻
+        """
+        news_word = ['工信部','失业率','进口','感染','疫苗','美联储','就业','天齐',
+                                      '锂','美国','欧盟','铁矿','原油','特斯拉','比亚迪','央行',
+                                      '住房和城乡建设部','自贸区','商务部','铁路','保监会','新能源',
+                                      '乘联会','大湾区','确诊','工业和信息化部','发改委','教育部',
+                                      '沪深','苹果','军事','住建部']
+        payload = {
+            "callback": "jQuery111204006017353852478_1630322898913",
+            "page": page,
+            "page_size": 20,
+            "zhibo_id": 152,
+            "tag_id": 0,
+            "dire": "f",
+            "dpc": 1,
+            "pagesize": 1000,
+            "_": "1630322898914",
+        }
+        r = requests.get(url="https://zhibo.sina.com.cn/api/zhibo/feed", params=payload)
+        news_list_str = str(r.text).partition('jQuery111204006017353852478_1630322898913(')[2].partition(');}')[0]
+        news_list_dict = ast.literal_eval(news_list_str)
+        if news_list_dict['result']['status']['msg'] != 'OK':
+            print('此次获取的列表为空')
+        else:
+            news_list = []
+            news_data_list = news_list_dict['result']['data']['feed']['list']
+            for i in news_data_list:
+                for key_word in news_word:
+                    if i['rich_text'].find(key_word) >=0:
+                        print(i['rich_text'])
+                        print(i['create_time'])
+        # return news_list
+
+if __name__ == '__main__':
+    a = news_finance()
+    for i in range(1, 50):
+        a.get_news(i)
